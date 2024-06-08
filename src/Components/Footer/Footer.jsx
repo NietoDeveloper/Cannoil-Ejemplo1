2212506c7fd9246ca96f9289e4db924ee234c32c
Manuel Nieto <nietosoftwaredeveloper@outlook.com>
2024-06-08 01:25:40 -0500

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,16 +15,16 @@ const Footer = () => {
                 ))}
             </article>
             <article className='flex flex-col items-center text-sm md:text-md'>
-                <h2 className='top-link md:hidden mb-1 md:mb-3'><a href='#navbar-mobile'>To The Top</a></h2>
-                <h2 className='top-link hidden md:block mb-1 md:mb-3'><a href='#navbar'>To The Top</a></h2>
+                <h2 className='top-link md:hidden mb-1 md:mb-3'><a href='#navbar-mobile'>Ir Arriba</a></h2>
+                <h2 className='top-link hidden md:block mb-1 md:mb-3'><a href='#navbar'>Ir Arriba</a></h2>
                 <h2 className='mailto-link mb-1 md:mb-3'> <button onClick={() => setTimeout(() =>
-                    window.open('mailto:kurzawa1@yahoo.com')
-                    , 500)} >Email Me</button></h2>
+                    window.open('mailto:NietoSoftWareDeveloper@outlookcom')
+                    , 500)} >Email CannOil</button></h2>
                 <h2 className='phone-link mb-1 md:mb-3'><button onClick={() => setTimeout(() =>
                     window.open('tel:2485682806')
-                    , 500)} >Call Me</button></h2>
-                <p className='mb-1 md:mb-3'>Copyright &copy; {today.getFullYear()} Kriskurzawadev - All Rights Reserved</p>
-                <h2 className='webdev-link mb-4 md:mb-8'>Site Designed and Built by Kris Kurzawa</h2>
+                    , 500)} >Mensaje Texto a CannOil</button></h2>
+                <p className='mb-1 md:mb-3'>CopyRights &copy; {today.getFullYear()} Todos Los Derechos Reservados</p>
+                <h2 className='webdev-link mb-4 md:mb-8'>Diseñado y Consruido por Nieto SoftWare Developer</h2>
             </article>
 
         </footer>
