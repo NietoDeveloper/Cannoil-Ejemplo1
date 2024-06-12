021f8e2c996a36fe51a662c81f428ad8e13a7c16
Manuel Nieto <nietosoftwaredeveloper@outlook.com>
2024-06-11 20:25:05 -0500

    ok

--- src/Components/Tech/Tech.jsx
+++ src/Components/Tech/Tech.jsx
@@ -5,6 +5,7 @@ const Techs = () => {
     return (
         <div>
             <h2 className='tech-header text-center text-3xl font-bold'>Nuestras Marcas</h2>
+            <img></img>
         </div>
     )
 }
