ca0e75686575897aa6543ff833b9fee832b808e6
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-09 00:16:18 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Outlet } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  
+
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
